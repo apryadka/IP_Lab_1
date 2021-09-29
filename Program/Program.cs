16ecf85f807f8cb4a92ad8f69c48cf2fb8fdfdc6
apryadka <todoalextodo@gmail.com>
2021-09-29 17:37:16 +0300

    exercise 1

---
+++ Program/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using SuperLibrary;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1) Функцiя знаходження ваги в повних тонах, якщо вона вiдома в кг.");
+            double kilograms = enter("Введiть кг: ");
+            double tons = Lib.ConvertKilogramToTonne(kilograms);
+            Console.WriteLine($"Вага у тонах: {tons}");
+            Console.WriteLine();
+
+
+        }
+        
+        public static double enter(string text)
+        {
+            double n;
+            Console.Write(text);
+            while (!double.TryParse(Console.ReadLine(), out n))
+            {
+                Console.WriteLine("Невірні дані");
+                Console.Write("Спробуйте ще раз : ");
+            }
+            return n;
+        }   
+    }
+}
---
+++ SuperLibrary/Lib.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SuperLibrary
+{
+    public class Lib
+    {
+        /// <summary>
+        /// converts a value in kilograms to tons.
+        /// </summary>
+        /// <param name="kilogram">number of kilograms</param>
+        /// <returns>number of tons</returns>
+        public static double ConvertKilogramToTonne(double kilogram)
+        {
+            try
+            {
+                if (kilogram > 0.0)
+                {
+                    return Convert.ToDouble(kilogram / 1000);
+                }
+                else { return 0.0; }
+            }
+            catch (ArgumentException e)
+            {
+                throw new ArgumentException(e.StackTrace);
+            }
+          
+        }
+    }
+}
